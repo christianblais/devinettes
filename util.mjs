35ced6359aafd7c5aa28c06e5a54c0cda66ab825
Christian Blais <christ.blais@gmail.com>
2024-09-12 20:54:37 -0400

    Split in multiple files

---
+++ util.mjs
@@ -0,0 +1,13 @@
+/**
+* @returns A new randomized array
+*/
+Array.prototype.toShuffled = function() {
+ const array = [...this];
+
+ for (let i = array.length - 1; i > 0; i--) {
+   const rand = Math.floor(Math.random() * (i + 1));
+   [array[i], array[rand]] = [array[rand], array[i]];
+ }
+
+ return array;
+}
